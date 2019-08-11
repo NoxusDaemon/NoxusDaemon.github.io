1c074e3c1ed101c2b53a7933a3e514f540cd63d3
NoxusDaemon <wahab_211715@hotmail.co.uk>
2019-08-11 21:21:35 +0100

    readded index files

---
+++ build/precache-manifest.331cefc64b7ad52e24966d5936f34a0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88792178f704662e27dd67ee2f7933ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3fa904ed27c7aaeb1d",
+    "url": "/static/css/main.697e29ad.chunk.css"
+  },
+  {
+    "revision": "39dc8f5eb3c3d7355761",
+    "url": "/static/js/2.edc93cb4.chunk.js"
+  },
+  {
+    "revision": "7e3fa904ed27c7aaeb1d",
+    "url": "/static/js/main.46e20914.chunk.js"
+  },
+  {
+    "revision": "3eb34a3da33091c00aa4",
+    "url": "/static/js/runtime~main.23afda1b.js"
+  }
+]);
